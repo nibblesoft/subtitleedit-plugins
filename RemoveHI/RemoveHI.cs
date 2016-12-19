4128944dfac350adae7582fdfe28c33e7bce276d
Ivandro Ismael <ivandrofly@gmail.com>
2016-12-19 15:38:08 +0000

    add project files.

---
+++ RemoveHI/RemoveHI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nikse.SubtitleEdit.PluginLogic;
+
+namespace Nibblesoft.SubtitleEdit.Plugins
+{
+    // 
+    public class RemoveHI : IPlugin
+    {
+        public string ActionType => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public string Shortcut => throw new NotImplementedException();
+
+        public string Text => throw new NotImplementedException();
+
+        public decimal Version => throw new NotImplementedException();
+
+        public string DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
